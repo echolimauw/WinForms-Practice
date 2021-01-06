f050f38cea98bec1109865216774357ce14f0483
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2021-01-05 16:36:35 -0800

    Adding walls.
    Still working on for loops used to render brick walls in form1.cs

--- SnakeGame/SnakeGame/Form1.cs
+++ SnakeGame/SnakeGame/Form1.cs
@@ -50,7 +50,23 @@ namespace SnakeGame
 
         private void frmSnake_Load(object sender, EventArgs e)
         {
+            GameBoard.Image = new Bitmap(420, 420);
+            g = Graphics.FromImage(GameBoard.Image);
+            g.Clear(Color.White);
 
+            for(int i = 1; i <= 10; i++)
+            {
+                // top and bottom walls
+                g.DrawImage(imgList.Images[6], i * 35, 0);
+                g.DrawImage(imgList.Images[6], i * 35, 385);
+            }
+
+            for(int i = 0; i <= 11; i++)
+            {
+                // top and bottom walls
+                g.DrawImage(imgList.Images[6], 0, i * 35);
+                g.DrawImage(imgList.Images[6], 385, i * 35);
+            }
         }
     }
 }
